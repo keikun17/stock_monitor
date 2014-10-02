054f4d4fb8f2b5bb0c890ebef5498f34dfd366a0
Buddy Magsipoc <keikun17@gmail.com>
2014-10-02 22:15:24 +0800

    Remove unused tests.

--- test/models/stock_field_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StockFieldTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/stock_template_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StockTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
