b2e51e859f8bb609ec456861d8663fac3fd8a748
Buddy Magsipoc <keikun17@gmail.com>
2014-10-03 22:38:25 +0800

    Relations.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,5 @@
 class Stock < ActiveRecord::Base
   belongs_to :stock_template
+  has_many :stock_fields
+  has_many :template_fields, through: :stock_template
 end
--- app/models/stock_template.rb
+++ app/models/stock_template.rb
@@ -1,2 +1,4 @@
 class StockTemplate < ActiveRecord::Base
+  has_many :stocks
+  has_many :template_fields
 end
