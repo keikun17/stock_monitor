62b9a792946a22d5ec039e77796bd2904347c0cb
Buddy Magsipoc <keikun17@gmail.com>
2014-10-07 23:14:55 +0800

    Fields for string field type.

--- spec/features/stock_management_spec.rb
+++ spec/features/stock_management_spec.rb
@@ -10,6 +10,7 @@ feature "Stock Management" do
     create(:product_field, name: 'ply', field_type: 'float', product: ar_belt)
     create(:product_field, name: 'Top Cover', field_type: 'float',  product: ar_belt)
     create(:product_field, name: 'Bottom Cover', field_type: 'float', product: ar_belt)
+    create(:product_field, name: 'Resistance', field_type: 'string', product: ar_belt)
   end
 
   scenario "Create a Stock Record" do
@@ -26,6 +27,7 @@ feature "Stock Management" do
     fill_in "ply", with: '3'
     fill_in "Top Cover", with: '4'
     fill_in "Bottom Cover", with: '2'
+    fill_in "Resistance", with: 'Abrasive'
     click_button "Create Stock"
 
     expect(page).to have_text("Stock was successfully created.")
