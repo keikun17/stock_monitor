769d79894ccb1a56a62ea3d25dd3b47c5f0ff9d3
Buddy Magsipoc <keikun17@gmail.com>
2014-10-08 00:45:29 +0800

    links.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -31,7 +31,7 @@ class StocksController < ApplicationController
 
     respond_to do |format|
       if @stock.save
-        format.html { redirect_to @stock, notice: 'Stock was successfully created.' }
+        format.html { redirect_to product_stock_path(@stock.product_id, @stock), notice: 'Stock was successfully created.' }
         format.json { render :show, status: :created, location: @stock }
       else
         format.html { render :new }
