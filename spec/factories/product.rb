1dc2713dfda1272cd30dcb5a5dcf448b9c6b5f5d
Buddy Magsipoc <keikun17@gmail.com>
2014-10-09 22:14:14 +0800

    [ci skip] Pending specs for stock movement.

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -1,5 +1,17 @@
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :product, class: Product do
-    name "Conveyor Belt"
+    name "Awesome Product"
   end
+
+
+  factory :abrasive_resistant_belt, class: Product do
+    name "Abrasive Resistant Conveyor Belt"
+
+    after :create do |product_field|
+    end
+
+  end
+
 end
+
+
