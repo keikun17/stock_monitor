cfc447bb0835de0f23639923d58dff933bb2b034
Buddy Magsipoc <keikun17@gmail.com>
2014-10-10 23:40:12 +0800

    Annotate.

--- spec/models/stock_field_spec.rb
+++ spec/models/stock_field_spec.rb
@@ -27,3 +27,17 @@ describe StockField do
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: stock_fields
+#
+#  id               :integer          not null, primary key
+#  stock_id         :integer
+#  float_value      :float
+#  string_value     :string
+#  text_value       :text
+#  product_field_id :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
