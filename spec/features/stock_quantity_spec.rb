582880902f273ed26b6e3be35fede38dcdf8ac2f
Buddy Magsipoc <keikun17@gmail.com>
2014-10-16 11:16:05 +0800

    Deposit creation.

--- spec/features/stock_quantity_spec.rb
+++ spec/features/stock_quantity_spec.rb
@@ -13,10 +13,9 @@ feature "Stock Quantity" do
 
     fill_in "Quantity", with: 555
 
-    expect(page.find('#deposit_stock_id').value).to eq(stock.id.to_s)
-    select Time.now.year, from: "deposit_date_ordered_1i"
-    select Time.now.strftime("%B"), from: "deposit_date_ordered_2i"
-    select Time.now.day, from: "deposit_date_ordered_3i"
+    # select Time.now.year, from: "deposit_date_ordered_1i"
+    # select Time.now.strftime("%B"), from: "deposit_date_ordered_2i"
+    # select Time.now.day, from: "deposit_date_ordered_3i"
     click_button "Create Deposit"
 
     expect(page).to have_text("Deposit was successfully created.")
@@ -24,7 +23,24 @@ feature "Stock Quantity" do
     expect(page).to have_text("555.0")
   end
 
-  scenario "Withdrawing decreases the stock quantity"
+  scenario "Withdrawing decreases the stock quantity" do
+    create(:deposit, quantity: 1000, stock: stock)
+
+    visit products_path
+    click_link "Abrasive Resistant Conveyor Belt"
+    click_link stock.id.to_s
+
+
+    click_link "Withdraw Stock"
+
+    fill_in "Quantity", with: 500
+
+    click_button "Create Withdraw"
+
+    expect(page).to have_text("Withdraw was successfully created.")
+    expect(stock.remaining_quantity).to eq(500.0)
+    expect(page).to have_text("500.0")
+  end
 
   scenario "A stock that has not arrived will not decrease the quantity"
 
